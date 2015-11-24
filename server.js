33fa930c46fc94d27d6835bfe623d79b53b2d6d3
Isao Hayakawa <amac0183@gmail.com>
2015-11-23 23:29:32 -0500

    Adding front end files, including index.html, bower.json, bootstrap, and jquery.

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
+var bodyParser = require('body-parser');
 var express    = require('express');
 var sqlite3    = require('sqlite3');
 
 var db = new sqlite3.Database('parking_violations.db');
 var app = express();
 
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({'extended':'true'}));
+app.use(bodyParser.json());
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+
 var server = app.listen(3000, function() {
     var address = server.address();
     console.log("Server listening to %s:%d within %s environment",
                 address.address, address.port, app.get('env'));
 });
-
